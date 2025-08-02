7bb741ed59d588bd55c9df4638a7cb0de37c2029
cotaByte <minanajoverrafa@gmail.com>
2025-08-02 15:56:35 +0200

    refactor/ generic type for debug operator

--- src/app/shared/operators.ts
+++ src/app/shared/operators.ts
@@ -1,7 +1,7 @@
 import { Observable, tap } from 'rxjs';
 
-export function debug(log?: string) {
-  return (source: Observable<any>) =>
+export function debug<T>(log?: string) {
+  return (source: Observable<T>) =>
     source.pipe(
       tap((value) => {
         console.log(log ?? '', value);
